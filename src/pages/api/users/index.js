e8e2e2e6b5fe72452eb9426cb231c5b4f4202faa
Keshav S <s.keshav13142@gmail.com>
2023-01-24 13:48:46 +0530

    Added dev endpoint to fetch all users

---
+++ src/pages/api/users/index.js
@@ -0,0 +1,11 @@
+import prisma from "@/lib/prisma";
+
+export default async function handler(req, res) {
+  //Return error if admin secret is missing
+  if (req.body.secret !== process.env.ADMIN_SECRET) {
+    res.status(500).json({ error: "Unauthorized" });
+    return;
+  }
+
+  res.json(await prisma.user.findMany());
+}
