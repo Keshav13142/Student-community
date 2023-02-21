09f3934669a2df23c217915d859981590469e91c
Keshav S <s.keshav13142@gmail.com>
2023-02-21 12:26:38 +0530

    Removed dev log statement

--- src/pages/api/institution/manage/admins.js
+++ src/pages/api/institution/manage/admins.js
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
   }
 
   const { institutionId, userId, action } = req.body;
-  console.log(req.body);
 
   if (!institutionId || !userId || !action) {
     res.status(500).json({ error: "Missing details!!" });
