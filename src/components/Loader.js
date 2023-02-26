f4325fb7a54d7f43b62e4e6e3b6778587f344efe
Keshav S <s.keshav13142@gmail.com>
2023-02-26 21:17:51 +0530

    Replaced react-lottie

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,19 +1,7 @@
-import Lottie from "react-lottie";
-import animationData from "../lotties/loading.json";
-
 export default function Loader() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <Lottie options={defaultOptions} height={300} width={300} />
+      <img src="/rings.svg" alt="loading" className="w-36" />
     </div>
   );
 }
