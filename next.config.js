c06c313ab7da414233f50e1f28e47ce368fe3ce2
Keshav S <s.keshav13142@gmail.com>
2023-03-18 13:52:24 +0530

    Updated next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "doodleipsum.com",
+      "illustrations.popsy.co",
+      "cdn-icons-png.flaticon.com",
+    ],
+  },
 };
 
 module.exports = nextConfig;
