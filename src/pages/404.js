ba667d755646e6bdef217e2d5b8afcc1912d047d
Keshav S <s.keshav13142@gmail.com>
2023-03-21 18:25:23 +0530

    Added custom 404 and 500 pages

---
+++ src/pages/404.js
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <>
+      <Head>
+        <title>Student community</title>
+        <meta
+          name="description"
+          content="Platform for students within institutions to interact"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <div className="flex min-h-screen flex-col items-center justify-center gap-5">
+        <Image
+          src="https://illustrations.popsy.co/violet/falling.svg"
+          alt="404"
+          width={300}
+          height={300}
+        />
+        <h1 className="mx-10 text-center text-xl font-semibold text-slate-700 md:text-2xl">
+          The page you&apos;re looking for can&apos;t be found.
+        </h1>
+        <Link href="/">
+          <button
+            type="button"
+            className="flex items-center gap-2 rounded-md border-2 border-purple-300 px-2 py-1 text-lg md:px-3 md:py-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+              />
+            </svg>
+            <span className="font-medium text-slate-500">Back to home</span>
+          </button>
+        </Link>
+      </div>
+    </>
+  );
+}
