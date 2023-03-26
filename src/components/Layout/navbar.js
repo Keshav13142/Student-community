a591dcd83cb0cd44fda1bc89ade72fb6fb017fcf
Keshav S <s.keshav13142@gmail.com>
2023-03-26 17:25:27 +0530

    Move edit profile compo from navbar to profile page

--- src/components/Layout/navbar.js
+++ src/components/Layout/navbar.js
@@ -1,13 +1,11 @@
 import {
   Avatar,
-  Button,
   IconButton,
   Menu,
   MenuButton,
   MenuItem,
   MenuList,
   useColorMode,
-  useDisclosure,
 } from "@chakra-ui/react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
@@ -16,17 +14,13 @@ import { BiLogOut } from "react-icons/bi";
 import { BsFillSunFill } from "react-icons/bs";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import { MdDarkMode } from "react-icons/md";
-import { SlSettings } from "react-icons/sl";
-import EditProfile from "../modals/edit-profile";
 
 const Navbar = ({ onSidebarOpen, showCommunityInfo }) => {
   const session = useSession();
-  const { isOpen, onClose, onOpen } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <>
-      <EditProfile isOpen={isOpen} onClose={onClose} />
       <nav className="flex items-center justify-between border-b border-b-slate-300 p-2 shadow-md dark:border-b-slate-700 md:px-6">
         <div className="flex items-center gap-2">
           <div
@@ -83,10 +77,6 @@ const Navbar = ({ onSidebarOpen, showCommunityInfo }) => {
                   <span>My Profile</span>
                 </MenuItem>
               </Link>
-              <MenuItem className="flex items-center gap-2" onClick={onOpen}>
-                <SlSettings />
-                <span>Settings</span>
-              </MenuItem>
               <MenuItem
                 className="flex items-center gap-2"
                 onClick={toggleColorMode}
--- src/pages/user/[username].js
+++ src/pages/user/[username].js
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import prisma from "@/lib/prisma";
+import EditProfile from "@/src/components/modals/edit-profile";
 import {
   Alert,
   AlertIcon,
@@ -11,6 +12,7 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { format } from "date-fns";
 import { getServerSession } from "next-auth";
@@ -18,6 +20,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { BiEdit } from "react-icons/bi";
 import { FiEdit } from "react-icons/fi";
 import { GoMarkGithub } from "react-icons/go";
 import { GrLinkedin } from "react-icons/gr";
@@ -125,6 +128,8 @@ export async function getServerSideProps({ req, res, query }) {
 
 const UserProfile = ({ profile, communities, ownProfile }) => {
   const router = useRouter();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <>
       <Head>
@@ -140,7 +145,8 @@ const UserProfile = ({ profile, communities, ownProfile }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div>
+      <div className="flex items-center justify-center px-2 py-5 pt-10 lg:px-10">
+        <EditProfile isOpen={isOpen} onClose={onClose} />
         {!profile ? (
           <div className="flex flex-col items-center justify-center px-10 pt-10">
             {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -160,138 +166,8 @@ const UserProfile = ({ profile, communities, ownProfile }) => {
             </Alert>
           </div>
         ) : (
-          <div className="flex flex-col items-center gap-10 rounded-xl px-2 py-5 pt-10 lg:flex-row lg:items-start lg:justify-center lg:px-10">
-            <div className="order-2 flex min-w-[50%] max-w-4xl flex-col gap-5 lg:order-1">
-              <h1 className="hidden text-2xl font-medium lg:block">
-                {profile.name}
-              </h1>
-              <Tabs
-                variant="line"
-                className="min-w-[90dvw] max-w-[90dvw] sm:min-w-[80dvw] lg:min-w-[0]"
-              >
-                <TabList>
-                  <Tab>Posts</Tab>
-                  <Tab>Communities</Tab>
-                </TabList>
-                <TabPanels>
-                  <TabPanel className="flex max-h-[60dvh] flex-col gap-5 overflow-y-auto">
-                    {profile.posts.length > 0 ? (
-                      profile.posts.map((p, idx) => (
-                        <div
-                          className="flex items-center justify-between gap-3 rounded-lg border border-slate-300 px-4 py-2 dark:border-slate-500"
-                          key={idx}
-                        >
-                          <div className="grow">
-                            <div className="mb-2 flex flex-col gap-1 md:gap-2 lg:gap-3">
-                              <div className="flex items-center gap-2">
-                                <Link
-                                  href={
-                                    p.published
-                                      ? `/blog/${p.slug}`
-                                      : `/blog/${p.id}/edit`
-                                  }
-                                  className="hover:underline"
-                                >
-                                  <h2 className="text-base font-medium text-slate-900 dark:text-slate-300 lg:text-lg">
-                                    {p.title}
-                                  </h2>
-                                </Link>
-                                {!p.published ? (
-                                  <Badge colorScheme="purple">Draft</Badge>
-                                ) : (
-                                  ownProfile && (
-                                    <IconButton
-                                      icon={<FiEdit />}
-                                      size="xs"
-                                      onClick={() =>
-                                        router.push(`/blog/${p.id}/edit`)
-                                      }
-                                      bg="transparent"
-                                    />
-                                  )
-                                )}
-                              </div>
-                              <h3 className="max-w-[30dvw] text-sm text-slate-500 line-clamp-2 dark:text-slate-400 lg:text-base">
-                                {p.content}
-                              </h3>
-                            </div>
-                            <div className="mt-2 flex items-center gap-2">
-                              <span className="text-xs text-gray-500 lg:text-sm">
-                                {format(new Date(p.createdAt), "MMM d")}
-                              </span>
-                            </div>
-                          </div>
-                          <Link
-                            href={
-                              p.published
-                                ? `/blog/${p.slug}`
-                                : `/blog/${p.id}/edit`
-                            }
-                          >
-                            {p.bannerImage === "" || !p.bannerImage ? (
-                              <Image
-                                src="https://cdn-icons-png.flaticon.com/512/3875/3875148.png"
-                                height="96"
-                                width="128"
-                                className="max-h-[6em] max-w-[6em] object-cover"
-                                alt="No img"
-                              />
-                            ) : (
-                              <img
-                                src={p.bannerImage}
-                                className="aspect-video h-24 w-32 rounded-md object-cover"
-                                alt={p.title}
-                              />
-                            )}
-                          </Link>
-                        </div>
-                      ))
-                    ) : (
-                      <div className="mt-3 flex flex-col items-center gap-2">
-                        <h2 className="text-center text-lg font-medium text-slate-600 lg:text-lg">
-                          {ownProfile ? "You have" : "This user has"} not
-                          authored any posts
-                        </h2>
-                        <Image
-                          src="https://illustrations.popsy.co/violet/falling.svg"
-                          className="object-cover"
-                          height={200}
-                          width={200}
-                          alt="No img"
-                        />
-                      </div>
-                    )}
-                  </TabPanel>
-                  <TabPanel className="flex max-h-[60dvh] flex-col gap-3 overflow-y-auto">
-                    {communities.map((c, idx) => (
-                      <div
-                        key={idx}
-                        className="rounded-lg border border-slate-300 dark:border-slate-500"
-                      >
-                        <Link href={`/community/${c.id}`}>
-                          <div className="flex items-center gap-2 rounded-xl p-2">
-                            <Avatar
-                              src={c.image}
-                              name={c.name}
-                              size={["sm", "md"]}
-                            />
-                            <div className="flex flex-col gap-2">
-                              <h4 className="text-base font-medium dark:text-slate-300 lg:text-lg">
-                                {c.name}
-                              </h4>
-                              <span className="text-sm dark:text-slate-400 lg:text-base">
-                                {c.desc}
-                              </span>
-                            </div>
-                          </div>
-                        </Link>
-                      </div>
-                    ))}
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
-            </div>
-            <div className="order-1 h-fit max-w-xl rounded-md border-2 border-purple-300 px-3 py-2 dark:border-slate-500 dark:text-slate-300 lg:order-2">
+          <div className="flex w-full max-w-[90dvw] flex-col gap-10 md:max-w-[75dvw] lg:max-w-[60dvw] xl:max-w-[45vw]">
+            <div className="rounded-md border-2 border-slate-200 px-3 py-2 dark:border-slate-700 dark:text-slate-300">
               <div className="flex flex-col gap-2">
                 <div className="flex items-center gap-2">
                   <Avatar
@@ -300,7 +176,12 @@ const UserProfile = ({ profile, communities, ownProfile }) => {
                     size={["sm", "md"]}
                   />
                   <div>
-                    <h3 className="text-xl font-medium">{profile.name}</h3>
+                    <div className="flex items-center gap-2">
+                      <h3 className="text-xl font-medium">{profile.name}</h3>
+                      {ownProfile && (
+                        <BiEdit className="cursor-pointer" onClick={onOpen} />
+                      )}
+                    </div>
                     <h4 className="text-purple-500 dark:text-purple-400">
                       @{profile.username}
                     </h4>
@@ -338,6 +219,129 @@ const UserProfile = ({ profile, communities, ownProfile }) => {
                   </div>
                 </div>
               </div>
+            </div>
+            <div className="flex grow flex-col">
+              <Tabs colorScheme="purple" isFitted variant="line">
+                <TabList>
+                  <Tab>Posts</Tab>
+                  <Tab>Communities</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel className="flex max-h-[60dvh] flex-col gap-5 overflow-y-auto">
+                    {profile.posts.length > 0 ? (
+                      profile.posts.map((p, idx) => (
+                        <div
+                          className="flex items-center justify-between gap-2 rounded-lg border-2 border-slate-200 px-4 py-2 dark:border-slate-700"
+                          key={idx}
+                        >
+                          <div className="flex flex-col gap-2">
+                            <div className="flex flex-col gap-1 md:gap-2 lg:gap-3">
+                              <div className="flex items-center gap-2">
+                                <Link
+                                  href={
+                                    p.published
+                                      ? `/blog/${p.slug}`
+                                      : `/blog/${p.id}/edit`
+                                  }
+                                  className="hover:underline"
+                                >
+                                  <h2 className="text-sm font-medium text-slate-900 dark:text-slate-300 lg:text-lg">
+                                    {p.title}
+                                  </h2>
+                                </Link>
+                                {!p.published ? (
+                                  <Badge colorScheme="purple">Draft</Badge>
+                                ) : (
+                                  ownProfile && (
+                                    <IconButton
+                                      icon={<FiEdit />}
+                                      size="xs"
+                                      onClick={() =>
+                                        router.push(`/blog/${p.id}/edit`)
+                                      }
+                                      bg="transparent"
+                                    />
+                                  )
+                                )}
+                              </div>
+                              <span className="block text-sm text-slate-500 line-clamp-2 dark:text-slate-400 lg:text-base">
+                                {p.content}
+                              </span>
+                            </div>
+                            <span className="font-mono text-xs font-medium text-gray-500 lg:text-sm">
+                              {format(new Date(p.createdAt), "MMM d")}
+                            </span>
+                          </div>
+                          <Link
+                            className="hidden sm:block"
+                            href={
+                              p.published
+                                ? `/blog/${p.slug}`
+                                : `/blog/${p.id}/edit`
+                            }
+                          >
+                            {p.bannerImage === "" || !p.bannerImage ? (
+                              <Image
+                                src="https://cdn-icons-png.flaticon.com/512/3875/3875148.png"
+                                height="96"
+                                width="128"
+                                className="max-h-[6em] max-w-[6em] object-cover"
+                                alt="No img"
+                              />
+                            ) : (
+                              <img
+                                src={p.bannerImage}
+                                className="aspect-video h-24 w-32 flex-1 rounded-md object-cover"
+                                alt={p.title}
+                              />
+                            )}
+                          </Link>
+                        </div>
+                      ))
+                    ) : (
+                      <div className="mt-3 flex flex-col items-center gap-2">
+                        <h2 className="text-center text-lg font-medium text-slate-600 lg:text-lg">
+                          {ownProfile ? "You have" : "This user has"} not
+                          authored any posts
+                        </h2>
+                        <Image
+                          src="https://illustrations.popsy.co/violet/falling.svg"
+                          className="object-cover"
+                          height={200}
+                          width={200}
+                          alt="No img"
+                        />
+                      </div>
+                    )}
+                  </TabPanel>
+                  <TabPanel className="flex max-h-[60dvh] flex-col gap-3 overflow-y-auto">
+                    {communities.map((c, idx) => (
+                      <div
+                        key={idx}
+                        className="rounded-lg border border-slate-300 dark:border-slate-500"
+                      >
+                        <Link href={`/community/${c.id}`}>
+                          <div className="flex items-center gap-2 rounded-xl p-2">
+                            <Avatar
+                              src={c.image}
+                              name={c.name}
+                              size={["sm", "md"]}
+                            />
+                            <div className="flex flex-col gap-2">
+                              <h4 className="text-sm font-medium dark:text-slate-300 lg:text-lg">
+                                {c.name}
+                              </h4>
+                              <span className="text-sm dark:text-slate-400 lg:text-base">
+                                {c.desc}
+                              </span>
+                            </div>
+                          </div>
+                        </Link>
+                      </div>
+                    ))}
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
             </div>
           </div>
         )}
